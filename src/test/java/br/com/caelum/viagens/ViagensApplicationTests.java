c4498a24a96ae2b5e36719e249c9b4e8e43e67ad
thaisandre <thaisandre.sc@gmail.com>
2020-02-07 13:49:39 -0300

    [Pais] testes de controller

--- src/test/java/br/com/caelum/viagens/ViagensApplicationTests.java
+++ src/test/java/br/com/caelum/viagens/ViagensApplicationTests.java
@@ -1,9 +1,11 @@
 package br.com.caelum.viagens;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 class ViagensApplicationTests {
 
 	@Test
---
+++ src/test/java/br/com/caelum/viagens/administrativo/controller/PaisControllerTests.java
@@ -0,0 +1,100 @@
+package br.com.caelum.viagens.administrativo.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.caelum.viagens.administrativo.controller.dto.input.NewPaisInputDto;
+import br.com.caelum.viagens.administrativo.model.Pais;
+import br.com.caelum.viagens.administrativo.repository.PaisRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+public class PaisControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private PaisRepository paisRepository;
+	@BeforeEach
+	
+	public void setUp() {
+		this.paisRepository.save(new Pais("Argentina"));
+		this.paisRepository.save(new Pais("Brasil"));
+	}
+	
+	@Test
+	public void deveSalvarNovoPaisQueAindaNaoExiste() throws Exception {
+		
+		NewPaisInputDto paisInputDto = new NewPaisInputDto();
+		paisInputDto.setNome("Chile");
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add(HttpHeaders.ACCEPT_LANGUAGE, "pt-BR");
+		
+		RequestBuilder request = post("/paises")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.headers(httpHeaders)
+				.content(new ObjectMapper().writeValueAsString(paisInputDto));
+				
+		mockMvc.perform(request)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.nome").value("Chile"));	
+	}
+	
+	@Test
+	public void deveRejeitarCadastroDePaisSemNome() throws Exception {
+		NewPaisInputDto paisInputDto = new NewPaisInputDto();
+		paisInputDto.setNome("");
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add(HttpHeaders.ACCEPT_LANGUAGE, "pt-BR");
+		
+		RequestBuilder request = post("/paises")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.headers(httpHeaders)
+				.content(new ObjectMapper().writeValueAsString(paisInputDto));
+				
+		mockMvc.perform(request)
+			.andExpect(jsonPath("$.fieldErrors").isArray())
+			.andExpect(jsonPath("$.fieldErrors[*].campo").value("nome"))
+			.andExpect(jsonPath("$.fieldErrors[*].mensagem").value("não pode estar em branco"));
+	}
+	
+	@Test
+	public void deveRejeitarCadastroDePaisQueJaExiste() throws Exception {
+		NewPaisInputDto paisInputDto = new NewPaisInputDto();
+		paisInputDto.setNome("Brasil");
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add(HttpHeaders.ACCEPT_LANGUAGE, "pt-BR");
+		
+		RequestBuilder request = post("/paises")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.headers(httpHeaders)
+				.content(new ObjectMapper().writeValueAsString(paisInputDto));
+				
+		mockMvc.perform(request)
+			.andExpect(jsonPath("$.fieldErrors").isArray())
+			.andExpect(jsonPath("$.fieldErrors[*].campo").value("nome"))
+			.andExpect(jsonPath("$.fieldErrors[*].mensagem").value("Pais já existe no sistema."));
+	}
+}
